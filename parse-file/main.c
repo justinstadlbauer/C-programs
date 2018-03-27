13289542a9d9f55f5dcb38910bc9265ed8d7702f
justinstadlbauer <justinstadlbauer@u.boisestate.edu>
2018-03-27 09:35:56 -0600

    Reads multiple files

--- parse-file/main.c
+++ parse-file/main.c
@@ -1,60 +1,96 @@
-/* Program that compares 2 files and prints 
-   the first line where they differ */
+/* Program that compares files and prints 
+   the number of times a test string from
+   stdin occurs in each file. Files are
+   supplied to the program as command-line 
+   arguments */
 
 #include <stdio.h>
 #include <string.h> 
 #include <stdlib.h>
-#include <errno.h>
 
 #define  BUF_LEN  64
- 
+
+void init_fp(int argc);
+FILE* newFile(void);
+
+FILE *file_array[64];
+char* quit = "exit";
+char in[BUF_LEN];
+char test[BUF_LEN];
+int found_it = 0;
+int n = 1;
+int k = 1;
+
 int main(int argc, char *argv[])
 {
-  FILE* fp;
-  char* quit = "exit";
-  char* input_file = argv[1];
-  char in[BUF_LEN];
-  char test[BUF_LEN];
-  int found_it = 0;
-  
-  if ((argc > 2) || (argc == 1))
-  {
-    printf("Usage: main [filename]\n");
-    exit(EXIT_FAILURE);
-  }
-
-  fp = fopen(input_file, "r");
-
-  if (fp == NULL)
-  {
-    if (argv[1] != NULL)
-    {
-      printf("error: %s does not exist\n", argv[1]);
-      exit(EXIT_FAILURE);
-    }
-  }
-  else
-  {
-    while (scanf("%s", test) == 1)
-    {
-      if ((strcmp(quit, test) == 0))
-      {
-	exit(EXIT_SUCCESS);
-      }
-
-      rewind(fp);
-      while (fscanf(fp,"%s",in) == 1)
-      {
-	if ((strcmp(in, test) == 0))
+	init_fp(argc-1);
+	
+	if (argc == 1)
 	{
-	  found_it++;
-	}  
-      }
-      printf("Number of \"%s\" in %s: %d\n",test, input_file, found_it);
-      found_it = 0;
-    }
-    fclose(fp);
-    return 0;
-  }
-} 
+		printf("Usage: main [filename] [filename] ...\n");
+		exit(EXIT_FAILURE);
+	}
 
+	int j;
+	for (j = 0; j < argc-1; j++)
+	{
+		file_array[j] = fopen(argv[n], "r");
+		
+		if (file_array[j] == NULL)
+		{
+			printf("error: %s does not exist\n",argv[n]);
+			exit(EXIT_FAILURE);
+		}
+		n++;
+	}
+  
+	while (scanf("%s", test) == 1)
+	{
+		if ((strcmp(quit, test) == 0))
+		{
+			exit(EXIT_SUCCESS);
+		}
+      
+		for (j = 0; j < argc-1; j++)
+		{
+			rewind(file_array[j]);
+		}
+
+		for (j = 0; j < argc-1; j++)
+		{
+			while (fscanf(file_array[j],"%s",in) == 1)
+			{
+				if ((strcmp(in, test) == 0))
+				{
+					found_it++;
+				}
+			}
+			printf("Number of \"%s\" in %s: %d\n", test, argv[k++], found_it);
+			found_it = 0;
+		}
+		k = 1;
+	}
+
+	for (j = 0; file_array[j] != NULL; j++)
+	{
+		fclose(file_array[j]);
+		free(file_array[j]);	    
+	}
+
+	return 0;
+  }
+
+
+FILE* newFile(void)
+{
+	return (FILE *)malloc(sizeof(FILE*));
+}
+
+void init_fp(int argc)
+{
+	int j;
+	for (j = 0; j < argc; j++)
+	{
+		file_array[j] = newFile();
+	}
+}
